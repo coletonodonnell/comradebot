e2ca8f447ca7d8c9ff715ff537580f8c0bd56014
Coleton O'Donnell <coleton.e.odonnell@gmail.com>
2020-09-20 00:05:00 +0000

    Testing Music (#2)

--- cogs/music.py
+++ cogs/music.py
@@ -1,14 +1,21 @@
+import subprocess
 import discord
+import datetime
 import os
+import glob
 import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.utils import get
 
 class Music(commands.Cog):
   load_dotenv("../.env")
   DJ = int(os.getenv('DJ_ROLE'))
   def __init__(self, client):
     self.client = client
+    self.client.loop.create_task(self.queue_system())
+    self.song_queue = asyncio.Queue()
+    self.next_queue = asyncio.Event() 
     self.SYSTEMPATH = os.getenv("SYSTEMPATH")
 
   # Voice Stuff:
@@ -25,43 +32,41 @@ class Music(commands.Cog):
     subprocess.run(['/usr/bin/youtube-dl', '-x', '--audio-format', 'mp3', '-o', title, link]) # Download the video via youtube-dl
     return title
 
-  # Queue system, uses asyncio.Event() and asyncio.Queue(). 
-  @staticmethod
-  async def queue_system():
+  # Queue system, uses asyncio.Event() and asyncio.Queue().
+  async def queue_system(self):
     while True: # While loop
-      next_queue.clear() # Set the flag to false
-      task = await song_queue.get() # Fetch most recent task (this is an array, where the 0th term is the voice_client object, and the second is the path to the video)
-      connected = task[0] # Fetch the 1st element 
+      self.next_queue.clear() # Set the flag to false
+      task = await self.song_queue.get() # Fetch most recent task (this is an array, where the 0th term is the voice_client object, and the second is the path to the video)
+      connected = task[0] # Fetch the 1st element
       path = task[1] # Fetch the 2nd element
-      await Music.player_queue(connected, path) # Throw these into player_queue()
-      await next_queue.wait() # The flag must be True for the next iteration to continue, this flag is set in player_queue()
+      await self.player_queue(connected, path) # Throw these into player_queue()
+      await self.next_queue.wait() # The flag must be True for the next iteration to continue, this flag is set in player_queue()
 
   # This is the actual task for the queue system, it plays the audio and when the audio is done playing, it sets the next_queue flag to true so that the while loop in queue_system() can finish
-  @staticmethod
-  async def player_queue(connected, path):
+  async def player_queue(self, connected, path):
     connected.play(discord.FFmpegPCMAudio(path)) # Play the audio
     playing = connected.is_playing() # define playing (This should always be true if the above happens without an error)
     while playing == True: # While loop, if playing is true, continue
-      playing = connected.is_playing() # Redefine playing, when the audio is done playing (it is set to False) the loop will finish 
+      playing = connected.is_playing() # Redefine playing, when the audio is done playing (it is set to False) the loop will finish
       await asyncio.sleep(0.1)
     if os.path.exists(path):
       os.remove(path)
-    song_queue.task_done() # This tells the queue that the task has finished completely, and that it can be removed from the dictionary
-    next_queue.set() # This sets the next_queue() to True. 
+    self.song_queue.task_done() # This tells the queue that the task has finished completely, and that it can be removed from the dictionary
+    self.next_queue.set() # This sets the next_queue() to True.
 
-  # Play command, this must have a link 
+  # Play command, this must have a link
   @commands.has_role(DJ)
   @commands.command(brief="Use this to play music!", help="Use ?play <YouTube Link>")
   async def play(self, ctx, link):
-    if Music.is_voice_connected(self, ctx) == True: # If is_voice_connected() == True then connected = ctx.voice_client
+    if self.is_voice_connected(ctx) == True: # If is_voice_connected() == True then connected = ctx.voice_client
       connected = ctx.voice_client
-    else: # Otherwise, please connect 
+    else: # Otherwise, please connect
       channel = ctx.author.voice.channel
       connected = await channel.connect()
 
     # Download the audio using link
     download = self.download_audio(link)
-    await song_queue.put([connected, download]) # Parse it into the queue
+    await self.song_queue.put([connected, download]) # Parse it into the queue
 
   @play.error
   async def play_error(self, ctx, error):
@@ -69,7 +74,7 @@ class Music(commands.Cog):
       message = await ctx.send("Sorry you don't have the DJ Role!")
       await asyncio.sleep(5)
       await message.delete()
-      
+
   # Join the voice channel, but don't play anything
   @commands.has_role(DJ)
   @commands.command(brief="This makes the bot join your Voice Channel")
@@ -104,7 +109,7 @@ class Music(commands.Cog):
     if Music.is_voice_connected(self, ctx) == True:
       connected = ctx.voice_client
       connected.stop()
-    else: 
+    else:
       ctx.send("The bot isn't even connected the voice channel! C'mon...")
 
   @skip.error
@@ -120,10 +125,10 @@ class Music(commands.Cog):
     if Music.is_voice_connected(self, ctx) == True:
       connected = ctx.voice_client
       connected.stop()
-      for x in range(song_queue.qsize()):
+      for x in range(self.song_queue.qsize()):
         connected.stop()
-        await song_queue.get() 
-        song_queue.task_done()
+        await self.song_queue.get()
+        self.song_queue.task_done()
       remove_files = glob.glob(f'{self.SYSTEMPATH}audio/*.mp3')
       for x in remove_files:
         os.remove(x)
