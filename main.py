dd1710bbf04b84e1d9baa7d6a0c276d6e4b8060b
coletonodonnell <coleton.e.odonnell@gmail.com>
2020-09-30 14:29:25 -0400

    Added load, unload and reload commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import time
 import os
+import os.path
 import json
 import asyncio
 from dotenv import load_dotenv
@@ -11,10 +12,42 @@ def main():
   TOKEN = os.getenv('DISCORD_TOKEN') # Bot Token
   GUILD = os.getenv('DISCORD_GUILD') # Guild
   SYSTEMPATH = os.getenv('SYSTEMPATH') # System Path
+  STAFF = int(os.getenv('STAFF_ROLE')) # Staff Role
+
 
   client = commands.Bot(command_prefix='?')
   client.remove_command('help')
   discord.Client.maximum_messages = 2000
+
+  @commands.has_role(STAFF)
+  @client.command()
+  async def load(ctx, extension):
+    if os.path.isfile(f"{SYSTEMPATH}cogs/{extension}.py"):
+      client.load_extension(f"cogs.{extension}")
+    else:
+      temp = ""
+      for filename in os.listdir(f"{SYSTEMPATH}cogs"):
+        if filename.endswith(".py"):
+          temp += f"`{filename[:-3]}`\n"
+      await ctx.send(f"{extension} doesn't exist, the possible cogs are:\n{temp[:-1]}")
+
+  @commands.has_role(STAFF)
+  @client.command()
+  async def unload(ctx, extension):
+    if os.path.isfile(f"{SYSTEMPATH}cogs/{extension}.py"):
+      client.unload_extension(f"cogs.{extension}")
+    else:
+      temp = ""
+      for filename in os.listdir(f"{SYSTEMPATH}cogs"):
+        if filename.endswith(".py"):
+          temp += f"`{filename[:-3]}`\n"
+      await ctx.send(f"{extension} doesn't exist, the possible cogs are:\n{temp[:-1]}")
+
+  @commands.has_role(STAFF)
+  @client.command()
+  async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
 
   # Checks if bot is connected to guild 
   @client.event
